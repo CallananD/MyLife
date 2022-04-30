60c68fdf11fee67b566114fe77ad4cd854dca134
CallananD <daniel.s.callanan@gmail.com>
2022-04-30 21:16:18 +0100

    updated home page

--- app.py
+++ app.py
@@ -6,28 +6,28 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+todo_db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
-    deadline = db.Column(db.Date)
+class Todo(todo_db.Model):
+    id = todo_db.Column(todo_db.Integer, primary_key=True)
+    title = todo_db.Column(todo_db.String(100))
+    complete = todo_db.Column(todo_db.Boolean)
+    deadline = todo_db.Column(todo_db.Date)
 
-db.create_all()
+todo_db.create_all()
 
 @app.get("/")
 def home():
-    todo_list = db.session.query(Todo).all()
+    todo_list = todo_db.session.query(Todo).all()
     return render_template("home.html",todo_list=todo_list)
 
 @app.get("/todo")
 def todo():
-    todo_list = db.session.query(Todo).all()
-    return render_template("base.html", todo_list=todo_list)
+    todo_list = todo_db.session.query(Todo).all()
+    return render_template("todo.html", todo_list=todo_list)
 
 @app.post("/add")
 def add():
@@ -38,29 +38,29 @@ def add():
                     deadline=datetime(int(deadline[0:4]),int(deadline[5:7]),int(deadline[8:10])))
     else:
         new_todo = Todo(title=title, complete=False, deadline=None)
-    db.session.add(new_todo)
-    db.session.commit()
+    todo_db.session.add(new_todo)
+    todo_db.session.commit()
     return redirect(url_for("todo"))
 
 @app.get("/update/<int:todo_id>")
 def update(todo_id):
-    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    todo = todo_db.session.query(Todo).filter(Todo.id == todo_id).first()
     todo.complete = not todo.complete
-    db.session.commit()
+    todo_db.session.commit()
     return redirect(url_for("todo"))
 
 @app.get("/delete/<int:todo_id>")
 def delete(todo_id):
-    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
+    todo = todo_db.session.query(Todo).filter(Todo.id == todo_id).first()
+    todo_db.session.delete(todo)
+    todo_db.session.commit()
     return redirect(url_for("todo"))
 
 events = [{'todo': 'Test events','date':'2022-04-30'}]
 @app.get("/calendar")
 def my_calendar():
     events = []
-    for event in db.session.query(Todo).all():
+    for event in todo_db.session.query(Todo).all():
         if event.complete == False:
             events.append({'todo':event.title,'deadline':event.deadline})
     return render_template("calendar.html", events=events)
