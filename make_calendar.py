c423c0e26bc09b17aead7e3a0d23116a615dfc2b
CallananD <daniel.s.callanan@gmail.com>
2022-04-30 12:57:32 +0100

    add .gitignore

--- make_calendar.py
+++
@@ -1,7 +0,0 @@
-import calendar
-
-calendar_example =  calendar.HTMLCalendar()
-calendar_file = open('templates/calendar.html', 'w')
-html_code = calendar_example.formatmonth(2021, 4)
-calendar_file.write(html_code)
-calendar_file.close()
